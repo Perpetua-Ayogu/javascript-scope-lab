e83989728d5e80fd64d9f8934941a4fe88bc8c2b
Perpetua Ayogu <perpyayogu@gmail.com>
2026-09-19 15:03:05 -0700

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Global scope
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+
+// Function
+function addBurger() {
+
+  // Function-scoped variable
+  var newBurger = "Flatburger";
+
+  burgers.push(newBurger);
+
+
+  // Block scope
+  if (true) {
+
+    const anotherNewBurger = "Maple Bacon Burger";
+
+    burgers.push(anotherNewBurger);
+  }
+}
+
+
+// Function that changes the global drink
+function changeFeaturedDrink() {
+
+  featuredDrink = "The JavaShake";
+}
